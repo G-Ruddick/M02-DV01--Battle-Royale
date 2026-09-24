c5ff9e317d19581b42839bf605833174098f3d72
Orangesuit1 <glen@ruddicks.net>
2026-09-23 21:59:57 -0700

    webgl build and bug fix

--- M02 DV01 Battle Royale/Assets/Scripts/NetworkManager.cs
+++ M02 DV01 Battle Royale/Assets/Scripts/NetworkManager.cs
@@ -31,6 +31,7 @@ public class NetworkManager : MonoBehaviourPunCallbacks {
         PhotonNetwork.JoinRoom(roomName);
     }
 
+    [PunRPC]
     public void ChangeScene(string sceneName) {
         PhotonNetwork.LoadLevel(sceneName);
     }
